bdd9e1e191e5733c02dc57a4cadd045415db4b61
Christian Evans <christian.evans214@gmail.com>
2015-07-04 13:03:22 -0400

    can remove choice from DB and started map marker functionality

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,153 +1,14 @@
-var styleArr = [
-    {
-        "featureType": "landscape",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 60
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 40
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.province",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": 30
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#ef8c25"
-            },
-            {
-                "lightness": 40
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#b6c54c"
-            },
-            {
-                "lightness": 40
-            },
-            {
-                "saturation": -40
-            }
-        ]
-    },
-    {}]
-function initialize_gmaps() {
-		    // initialize new google maps LatLng object
-		    var myLatlng = new google.maps.LatLng(40.705786,-74.007672);
-		    // set the map options hash
-		    var mapOptions = {
-		        center: myLatlng,
-		        zoom: 13,
-		        mapTypeId: google.maps.MapTypeId.ROADMAP,
-		        styles: styleArr
-		    };
-		    // get the maps div's HTML obj
-		    var map_canvas_obj = document.getElementById("map-canvas");
-		    // initialize a new Google Map with the options
-		    var map = new google.maps.Map(map_canvas_obj, mapOptions);
-		    // Add the marker to the map
-		    var marker = new google.maps.Marker({
-		        position: myLatlng,
-		        title:"Hello World!"
-		    });
-		    
-		    // draw some locations
-		    var hotelLocation = [40.705137, -74.007624];
-		    var restaurantLocations = [
-		        [40.705137, -74.013940],
-		        [40.708475, -74.010846]
-		    ];
-		    var thingToDoLocations = [
-		        [40.716291, -73.995315],
-		        [40.707119, -74.003602]
-		    ];
+var daysChoice = [{
+    hotel: [],
+    restaurant: [],
+    thing: [],
+    hotelLocations: [],
+    restaurantLocations: [],
+    thingLocations: []
+}];
 
-		    function drawLocation (location, opts) {
-		        if (typeof opts !== 'object') {
-		            opts = {}
-		        }
-		        opts.position = new google.maps.LatLng(location[0], location[1]);
-		        opts.map = map;
-		        var marker = new google.maps.Marker(opts);
-		    }
-		    drawLocation(hotelLocation, {
-		        icon: '/images/lodging_0star.png'
-		    });
-		    restaurantLocations.forEach(function (loc) {
-		        drawLocation(loc, {
-		            icon: '/images/restaurant.png'
-		        });
-		    });
-		    thingToDoLocations.forEach(function (loc) {
-		        drawLocation(loc, {
-		            icon: '/images/star-3.png'
-		        });
-		    });
-		}
+var arr = ['hotel', 'restaurant', 'thing'];
 
-var daysChoice = [{hotel:[],restaurant:[],thing:[]}];
-var latLongChoice = []
-var arr = ['hotel','restaurant','thing'];
 $(document).ready(function() {
     initialize_gmaps();
     arr.forEach(addChoice);
@@ -157,31 +18,148 @@ $(document).ready(function() {
     removeDay();
 });
 
-function addChoice(str){
-	$('#'+str+'Add').on('click',function(){
-		
-		var selection = $('#'+str+'Select').val();
-			if($('#'+str+'Added').text().indexOf(selection)===-1){
 
-				$('#'+str+'Added').append('<div class="itinerary-item"><span class="title">'+selection+'</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
-			     var indexOfDay = Number($('.current-day').text())-1;
-                 if(daysChoice[indexOfDay]===undefined) daysChoice[indexOfDay]={};
-                 // if(daysChoice[indexOfDay][str]===undefined) daysChoice[indexOfDay][str]=[];
-                 daysChoice[indexOfDay][str].push(selection);
-            };
+var styleArr = [{
+    "featureType": "landscape",
+    "stylers": [{
+        "saturation": -100
+    }, {
+        "lightness": 60
+    }]
+}, {
+    "featureType": "road.local",
+    "stylers": [{
+        "saturation": -100
+    }, {
+        "lightness": 40
+    }, {
+        "visibility": "on"
+    }]
+}, {
+    "featureType": "transit",
+    "stylers": [{
+        "saturation": -100
+    }, {
+        "visibility": "simplified"
+    }]
+}, {
+    "featureType": "administrative.province",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "featureType": "water",
+    "stylers": [{
+        "visibility": "on"
+    }, {
+        "lightness": 30
+    }]
+}, {
+    "featureType": "road.highway",
+    "elementType": "geometry.fill",
+    "stylers": [{
+        "color": "#ef8c25"
+    }, {
+        "lightness": 40
+    }]
+}, {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [{
+        "color": "#b6c54c"
+    }, {
+        "lightness": 40
+    }, {
+        "saturation": -40
+    }]
+}, {}]
 
-        })
+var map;
+
+function initialize_gmaps() {
+    // initialize new google maps LatLng object
+    var myLatlng = new google.maps.LatLng(40.705786, -74.007672);
+    // set the map options hash
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: styleArr
+    };
+    // get the maps div's HTML obj
+    var map_canvas_obj = document.getElementById("map-canvas");
+    // initialize a new Google Map with the options
+    map = new google.maps.Map(map_canvas_obj, mapOptions);
+    // Add the marker to the map
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        title: "Hello World!"
+    });
+
+    // draw some locations
+    var hotelLocation = [40.705137, -74.007624];
+    var restaurantLocations = [
+        [40.705137, -74.013940],
+        [40.708475, -74.010846]
+    ];
+    var thingToDoLocations = [
+        [40.716291, -73.995315],
+        [40.707119, -74.003602]
+    ];
+
+    function drawLocation(location, opts) {
+        if (typeof opts !== 'object') {
+            opts = {}
+        }
+        opts.position = new google.maps.LatLng(location[0], location[1]);
+        opts.map = map;
+        var marker = new google.maps.Marker(opts);
+    }
+    drawLocation(hotelLocation, {
+        icon: '/images/lodging_0star.png'
+    });
+    restaurantLocations.forEach(function(loc) {
+        drawLocation(loc, {
+            icon: '/images/restaurant.png'
+        });
+    });
+    thingToDoLocations.forEach(function(loc) {
+        drawLocation(loc, {
+            icon: '/images/star-3.png'
+        });
+    });
+}
+
+function addChoice(str) {
+    $('#' + str + 'Add').on('click', function() {
+
+        var selection = $('#' + str + 'Select').val();
+        //take lat/long from selection and add to locations array.
+        //console.dir(dataObj[str]);
+        //dataObj[str].forEach(function())
+        if ($('#' + str + 'Added').text().indexOf(selection) === -1) {
+            $('#' + str + 'Added').append('<div class="itinerary-item"><span class="title">' + selection + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+            var indexOfDay = Number($('.current-day').text()) - 1;
+            daysChoice[indexOfDay][str].push(selection);
+        };
+
+    })
 }
 
 
-//NEED TO REMOVE FROM "DATABASE" AFTER REMOVING
-function removeChoice(){
-    $('.panel-body').on('click', '.remove', function(){
+function removeChoice() {
+    $('.panel-body').on('click', '.remove', function() {
         var $this = $(this);
         var choiceType = $this.parent().parent().attr('class').split(" ")[1];
         var nameToRemove = $this.prev().text();
-        var dayIndex = Number($(".current-day").text())-1;
-        daysChoice[dayIndex][choiceType] = daysChoice[dayIndex][choiceType].filter(function(activity){
+        var dayIndex = Number($(".current-day").text()) - 1;
+        daysChoice[dayIndex][choiceType] = daysChoice[dayIndex][choiceType].filter(function(activity) {
             if (activity === nameToRemove) return false;
             return true;
         })
@@ -189,70 +167,58 @@ function removeChoice(){
     })
 }
 
-// function 
 
-function addDay(){
-    $('#add-btn').on('click', function(){
-
+function addDay() {
+    $('#add-btn').on('click', function() {
         var $before = parseInt($(this).prev().text());
-        daysChoice[$before]={hotel:[],restaurant:[],thing:[]};
+        daysChoice[$before] = {
+            hotel: [],
+            restaurant: [],
+            thing: []
+        };
         $before++;
-        //console.log($before);
 
-         $(this).before(' <button class="btn btn-circle day-btn day">'+ $before.toString() +'</button> ');
+        $(this).before(' <button class="btn btn-circle day-btn day">' + $before.toString() + '</button> ');
     })
 }
 
-function getCurrentDay(){
-    
-    $('.day-buttons').on('click','.day',function(){
+function getCurrentDay() {
+    $('.day-buttons').on('click', '.day', function() {
         $('.current-day').removeClass('current-day');
         $(this).addClass('current-day');
         $('#day-title span').text("Day " + $(this).text())
-        arr.forEach(function(s){
-            $('#'+s+'Added').empty();
-            daysChoice[Number($('.current-day').text())-1][s].forEach(function(q){
-                $('#'+s+'Added').append('<div class="itinerary-item"><span class="title">'+q+'</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+        arr.forEach(function(s) {
+            $('#' + s + 'Added').empty();
+            daysChoice[Number($('.current-day').text()) - 1][s].forEach(function(q) {
+                $('#' + s + 'Added').append('<div class="itinerary-item"><span class="title">' + q + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
             })
-
         });
     })
 }
 
-function removeDay(){
-    $(".remove-current").on('click',function(){
-        var indexArray = Number($('.current-day').text())-1;
-        daysChoice.splice(indexArray,1);
+function removeDay() {
+    $(".remove-current").on('click', function() {
         var $currentday = $('.current-day');
+        if (daysChoice.length === 1) return;
+        var indexArray = Number($currentday.text()) - 1;
+        daysChoice.splice(indexArray, 1);
         $currentday.removeClass('current-day')
-        if ($currentday.text() == "1"){
+        if ($currentday.text() == "1") {
             $currentday.next().addClass('current-day');
             shiftDays($currentday);
             $currentday.next().trigger('click');
-        }
-        else {
+        } else {
             $currentday.prev().addClass('current-day');
             shiftDays($currentday);
-            $currentday.prev().trigger('click');        
+            $currentday.prev().trigger('click');
         }
         $currentday.remove();
     })
 }
 
-function shiftDays($currentday){
-    Array.prototype.slice.call($currentday.nextAll()).forEach(function(sibling){
+function shiftDays($currentday) {
+    Array.prototype.slice.call($currentday.nextAll()).forEach(function(sibling) {
         var tempText = Number($(sibling).text());
         if (!isNaN(tempText)) $(sibling).text(--tempText);
     })
 }
-
-
-
-
-
-
-
-
-
-
-
